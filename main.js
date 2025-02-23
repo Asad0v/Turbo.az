70f10235a228320bfd348fb87cd2d815dfe61d0c
Rashid <reshid.asedov@div.edu.az>
2025-02-23 15:03:27 +0400

    second commit

--- main.js
+++ main.js
@@ -10,18 +10,18 @@ function handleToggler(status) {
 }
 function openBasket(status) {
     basket.style.right = status ? '0' : '-30vw'
-   
+
 }
 const arr = [
-    { id: 1, status:false, marka: 'Mercedes', qiymet: '10 500', model: 'w202', mator: '2.0', il: '1998', reng: 'silver', img: 'https://turbo.azstatic.com/uploads/full/2022%2F02%2F07%2F16%2F20%2F36%2F05ae3ec7-b845-4458-8033-99eec8aec1a2%2F83217_ryHX2Q7AONB3-jC_sEJ3mQ.jpg' },
-    { id: 2, status:false, marka: 'Kia', qiymet: '21 000', model: 'Rio', mator: '1.4', il: '2014', reng: 'silver', img: 'https://turbo.azstatic.com/uploads/full/2021%2F09%2F10%2F11%2F40%2F18%2Fbe262b8b-13ff-40ee-9664-4c7c4ea93e4b%2F94552_bf7HpneCobaNn8ooAalBwg.jpg' },
-    { id: 3, status:false, marka: 'Hyundai', qiymet: '12 000', model: 'Tucson', mator: '2.0', il: '2014', reng: 'black', img: 'https://turbo.azstatic.com/uploads/f660x496/2020%2F02%2F08%2F12%2F31%2F23%2Fdfd4bcdd-5279-4807-85cb-58aa153c900f%2F44290_Il5UqH7nhzVTrAAfdt5BuQ.jpg' },
-    { id: 4, status:false, marka: 'LADA (VAZ)', qiymet: '35 000', model: '2107', mator: '1.6', il: '2010', reng: 'black', img: 'https://turbo.azstatic.com/uploads/full/2022%2F09%2F29%2F04%2F48%2F26%2Ff85c3fb0-4f77-4157-8b15-a3122bbaf36d%2F15612_S_xRbeuG6jQhZhkc_JM3wg.jpg' },
-    { id: 5, status:false, marka: 'BMW', qiymet: '28 700', model: 'M5 e39', mator: '4.4', il: '1998', reng: 'blue', img: 'https://turbo.azstatic.com/uploads/full/2020%2F08%2F15%2F17%2F42%2F55%2F5e61d0d8-ac41-42d4-8a34-33f9c563bba8%2F4747_ZOpsrcdSRl_mByG6tDsiqQ.jpg' },
-    { id: 6, status:false, marka: 'Mercedes', qiymet: '86 500', model: 'CLS', mator: '5.5', il: '2013', reng: 'gray', img: 'https://turbo.azstatic.com/uploads/full/2021%2F12%2F09%2F01%2F51%2F22%2F2e3d6e19-baa7-4625-89e0-d358238808ce%2F21872_z_Z4BhoRifgwGf0Fz_hvJw.jpg' },
-    { id: 7, status:false, marka: 'Toyota', qiymet: '54 000', model: 'Camry', mator: '3.0', il: '2021', reng: 'white', img: 'https://turbo.azstatic.com/uploads/full/2021%2F06%2F18%2F16%2F16%2F02%2Fd4a556f8-adc6-4ff8-897b-2079ea777987%2F24971_KEan9mcELSSmUAPbxG1KEw.jpg' },
-    { id: 8, status:false, marka: 'Porche', qiymet: '118 400', model: '911 Carrera', mator: '4.4', il: '2023', reng: 'cyan', img: 'https://turbo.azstatic.com/uploads/full/2020%2F08%2F13%2F13%2F32%2F55%2F012a26df-efae-4cf0-b763-a4c3de17ae23%2F46349_cqQK5HyMFGcJOEMh1pCm1A.jpg' },
-    { id: 9, status:false, marka: 'Dodge', qiymet: '38 500', model: 'Challenger', mator: '7.2', il: '2018', reng: 'black', img: 'https://turbo.azstatic.com/uploads/full/2021%2F04%2F09%2F10%2F02%2F28%2F9a6bf393-9f08-476b-abc0-d6278f1d6668%2F37456_34ZTVBn26COoCWhsqBx3vw.jpg' },
+    { id: 1, status: false, marka: 'Mercedes', qiymet: '10 500', model: 'w202', mator: '2.0', il: '1998', reng: 'silver', img: 'https://turbo.azstatic.com/uploads/full/2022%2F02%2F07%2F16%2F20%2F36%2F05ae3ec7-b845-4458-8033-99eec8aec1a2%2F83217_ryHX2Q7AONB3-jC_sEJ3mQ.jpg' },
+    { id: 2, status: false, marka: 'Kia', qiymet: '21 000', model: 'Rio', mator: '1.4', il: '2014', reng: 'silver', img: 'https://turbo.azstatic.com/uploads/full/2021%2F09%2F10%2F11%2F40%2F18%2Fbe262b8b-13ff-40ee-9664-4c7c4ea93e4b%2F94552_bf7HpneCobaNn8ooAalBwg.jpg' },
+    { id: 3, status: false, marka: 'Hyundai', qiymet: '12 000', model: 'Tucson', mator: '2.0', il: '2014', reng: 'black', img: 'https://turbo.azstatic.com/uploads/f660x496/2020%2F02%2F08%2F12%2F31%2F23%2Fdfd4bcdd-5279-4807-85cb-58aa153c900f%2F44290_Il5UqH7nhzVTrAAfdt5BuQ.jpg' },
+    { id: 4, status: false, marka: 'LADA (VAZ)', qiymet: '35 000', model: '2107', mator: '1.6', il: '2010', reng: 'black', img: 'https://turbo.azstatic.com/uploads/full/2022%2F09%2F29%2F04%2F48%2F26%2Ff85c3fb0-4f77-4157-8b15-a3122bbaf36d%2F15612_S_xRbeuG6jQhZhkc_JM3wg.jpg' },
+    { id: 5, status: false, marka: 'BMW', qiymet: '28 700', model: 'M5 e39', mator: '4.4', il: '1998', reng: 'blue', img: 'https://turbo.azstatic.com/uploads/full/2020%2F08%2F15%2F17%2F42%2F55%2F5e61d0d8-ac41-42d4-8a34-33f9c563bba8%2F4747_ZOpsrcdSRl_mByG6tDsiqQ.jpg' },
+    { id: 6, status: false, marka: 'Mercedes', qiymet: '86 500', model: 'CLS', mator: '5.5', il: '2013', reng: 'gray', img: 'https://turbo.azstatic.com/uploads/full/2021%2F12%2F09%2F01%2F51%2F22%2F2e3d6e19-baa7-4625-89e0-d358238808ce%2F21872_z_Z4BhoRifgwGf0Fz_hvJw.jpg' },
+    { id: 7, status: false, marka: 'Toyota', qiymet: '54 000', model: 'Camry', mator: '3.0', il: '2021', reng: 'white', img: 'https://turbo.azstatic.com/uploads/full/2021%2F06%2F18%2F16%2F16%2F02%2Fd4a556f8-adc6-4ff8-897b-2079ea777987%2F24971_KEan9mcELSSmUAPbxG1KEw.jpg' },
+    { id: 8, status: false, marka: 'Porche', qiymet: '118 400', model: '911 Carrera', mator: '4.4', il: '2023', reng: 'cyan', img: 'https://turbo.azstatic.com/uploads/full/2020%2F08%2F13%2F13%2F32%2F55%2F012a26df-efae-4cf0-b763-a4c3de17ae23%2F46349_cqQK5HyMFGcJOEMh1pCm1A.jpg' },
+    { id: 9, status: false, marka: 'Dodge', qiymet: '38 500', model: 'Challenger', mator: '7.2', il: '2018', reng: 'black', img: 'https://turbo.azstatic.com/uploads/full/2021%2F04%2F09%2F10%2F02%2F28%2F9a6bf393-9f08-476b-abc0-d6278f1d6668%2F37456_34ZTVBn26COoCWhsqBx3vw.jpg' },
     { id: 10, status: false, marka: 'Nissan', qiymet: '86 000', model: 'GTR R-35', mator: '3.0', il: '2014', reng: 'black', img: 'https://turbo.azstatic.com/uploads/full/2017%2F02%2F16%2F00%2F02%2F00%2F573%2F54000_4wUHTVdrb_-kLx6BJMuF8Q.jpg' },
     { id: 11, status: false, marka: 'Audi', qiymet: '124 100', model: 'RS7', mator: '4.0', il: '2015', reng: 'dimgrey', img: 'https://turbo.azstatic.com/uploads/full/2021%2F05%2F29%2F15%2F33%2F07%2Fc9bec2ac-97eb-4e07-bc8a-509f1898dd7d%2F82001_MmzL0_nnwbSnJI6rtabq7Q.jpg' },
     { id: 12, status: false, marka: 'Toyota', qiymet: '15 800', model: 'Corolla', mator: '1.6', il: '2016', reng: 'white', img: 'https://turbo.azstatic.com/uploads/full/2020%2F09%2F24%2F08%2F21%2F40%2Facdc9e7e-d243-46fd-b2ce-dc51a85c8343%2F66667_OxPaAqSg8MzGM7diar5WlQ.jpg' },
@@ -30,22 +30,25 @@ const arr = [
 ]
 const marka = ['Bütün Avtomobillər']
 function makeOpt() {
-    arr.map(item =>{
+    arr.map(item => {
         if (!marka.includes(item.marka)) {
             marka.push(item.marka)
         }
     })
     for (let i = 0; i < marka.length; i++) {
         automarka.innerHTML += `<option class=" bg-red-700 text-center text-white">${marka[i]}</option>`
-        
+
     }
     console.log(marka);
 }
 makeOpt()
+let cardsNum = 4
 function printCards() {
     cards.innerHTML = ''
     let kod = ''
-    arr.map(item => kod += `<div class=" relative  max-w-xs   rounded-md shadow-md  dark:text-gray-900">
+    arr
+        .slice(0, cardsNum)
+        .map(item => kod += `<div class=" w-[290px] relative  max-w-xs   rounded-md shadow-md  dark:text-gray-900">
         <img src="${item.img}" alt="" class=" object-cover object-center w-full rounded-tl-md rounded-tr-md h-72 dark:bg-gray-500">
         <i  onclick="makeHeart(this)" class="heart fa-regular fa-heart absolute right-5 top-5 text-xl text-white cursor-pointer "></i>
         <div class="mt-2  bg-white  pl-3">
@@ -53,10 +56,61 @@ function printCards() {
         <h2 onclick="showDet(${item.id})" class="text-lg font-normal tracking-wide cursor-pointer">${item.marka} ${item.model}</h2>
         </div>
         <p class="dark:text-gray-800 pb-4 pl-3">${item.il}, ${item.mator}, <span style=" color: ${item.reng};">${item.reng}<span></p>
+        <button onclick="buy(${item.id})" class="mt-[20px] p-[10px] px-[30px] rounded-[10px] transition ease-in-out delay-150 bg-red-500 hover:-translate-y-1 hover:scale-110 hover:bg-red-800 hover:text-white duration-300">AL</button>
+
         </div>`)
     cards.innerHTML = kod
 }
 printCards()
+const basketArr = []
+const shoppingList = document.getElementById('shoppingList')
+function buy(id) {
+    const obj = arr.find(item => item.id == id)
+    basketArr.push(obj)
+    console.log(basketArr);
+    
+    shoppingList.innerHTML = ""
+
+   basketArr.forEach(item => shoppingList.innerHTML +=`
+		<li class="flex flex-col py-6 sm:flex-row sm:justify-between">
+			<div class="flex w-full space-x-2 sm:space-x-4">
+				<img class="flex-shrink-0 object-cover w-20 h-20 dark:border- rounded outline-none sm:w-32 sm:h-32 dark:bg-gray-500" src="${item.img}" alt="Polaroid camera">
+				<div class="flex flex-col justify-between w-full pb-4">
+					<div class="flex justify-between w-full pb-2 space-x-2">
+						<div class="space-y-1">
+							<h3 class="text-lg font-semibold leading-snug sm:pr-8">${item.marka} ${item.model}</h3>
+							<p class="text-sm dark:text-gray-600">${item.il}, ${item.mator}, <span style=" color: ${item.reng};">${item.reng}</p>
+						</div>
+						<div class="text-right">
+							<p class="text-lg font-semibold">${item.qiymet} AZN</p>
+							<p class="text-sm line-through dark:text-gray-400">Endirime ehtiyac yoxdu endirimli qiymetdide ele</p>
+						</div>
+					</div>
+					<div class="flex text-sm divide-x">
+						<button type="button" class="flex items-center px-2 py-1 pl-0 space-x-1">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-4 h-4 fill-current">
+								<path d="M96,472a23.82,23.82,0,0,0,23.579,24H392.421A23.82,23.82,0,0,0,416,472V152H96Zm32-288H384V464H128Z"></path>
+								<rect width="32" height="200" x="168" y="216"></rect>
+								<rect width="32" height="200" x="240" y="216"></rect>
+								<rect width="32" height="200" x="312" y="216"></rect>
+								<path d="M328,88V40c0-13.458-9.488-24-21.6-24H205.6C193.488,16,184,26.542,184,40V88H64v32H448V88ZM216,48h80V88H216Z"></path>
+							</svg>
+							<span>Remove</span>
+						</button>
+						<button type="button" class="flex items-center px-2 py-1 space-x-1">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-4 h-4 fill-current">
+								<path d="M453.122,79.012a128,128,0,0,0-181.087.068l-15.511,15.7L241.142,79.114l-.1-.1a128,128,0,0,0-181.02,0l-6.91,6.91a128,128,0,0,0,0,181.019L235.485,449.314l20.595,21.578.491-.492.533.533L276.4,450.574,460.032,266.94a128.147,128.147,0,0,0,0-181.019ZM437.4,244.313,256.571,425.146,75.738,244.313a96,96,0,0,1,0-135.764l6.911-6.91a96,96,0,0,1,135.713-.051l38.093,38.787,38.274-38.736a96,96,0,0,1,135.765,0l6.91,6.909A96.11,96.11,0,0,1,437.4,244.313Z"></path>
+							</svg>
+							<span>Add to favorites</span>
+						</button>
+					</div>
+				</div>
+			</div>
+            </li>
+            
+            `)
+            
+}
 
 function showDet(id) {
     const obj = arr.find(item => item.id == id)
@@ -92,9 +146,9 @@ function showDet(id) {
 
 }
 function printFilter() {
-    
-    const filterArr = arr.filter(item =>item.marka.toLocaleLowerCase().includes(inp.value.toLocaleLowerCase()))
-        let kod = filterArr.map(item => `<div class=" relative  max-w-xs   rounded-md shadow-md  dark:text-gray-900">
+
+    const filterArr = arr.filter(item => item.marka.toLocaleLowerCase().includes(inp.value.toLocaleLowerCase()))
+    let kod = filterArr.map(item => `<div class=" w-[290px] relative  max-w-xs   rounded-md shadow-md  dark:text-gray-900">
                                             <img src="${item.img}" alt="" class=" object-cover object-center w-full rounded-tl-md rounded-tr-md h-72 dark:bg-gray-500">
                                             <i  onclick="makeHeart(this)" class="heart fa-regular fa-heart absolute right-5 top-5 text-xl text-white cursor-pointer "></i>
                                             <div class="mt-2  bg-white  pl-3">
@@ -103,23 +157,23 @@ function printFilter() {
                                             </div>
                                             <p class="dark:text-gray-800 pb-4 pl-3">${item.il}, ${item.mator}, <span style=" color: ${item.reng};">${item.reng}<span></p>
                                         </div>`
-        )
-        
-        cards.innerHTML = kod
+    )
 
-        if(filterArr.length == 0){
-            cards.innerHTML = `<p class="text-lg font-bold" > Axtardiginiz Avtomobil satishda  yoxdur...</p>`
+    cards.innerHTML = kod
 
-        }
+    if (filterArr.length == 0) {
+        cards.innerHTML = `<p class="text-lg font-bold" > Axtardiginiz Avtomobil satishda  yoxdur...</p>`
+
+    }
 }
 
 function markaFilter() {
-    if(automarka.value == 'Bütün Avtomobillər'){
+    if (automarka.value == 'Bütün Avtomobillər') {
         printCards()
         return
     }
-    const filterArr = arr.filter(item =>item.marka.toLocaleLowerCase().includes(automarka.value.toLocaleLowerCase()))
-    let kod = filterArr.map(item => `<div class=" relative  max-w-xs   rounded-md shadow-md  dark:text-gray-900">
+    const filterArr = arr.filter(item => item.marka.toLocaleLowerCase().includes(automarka.value.toLocaleLowerCase()))
+    let kod = filterArr.map(item => `<div class=" w-[290px] relative  max-w-xs   rounded-md shadow-md  dark:text-gray-900">
         <img src="${item.img}" alt="" class=" object-cover object-center w-full rounded-tl-md rounded-tr-md h-72 dark:bg-gray-500">
         <i  onclick="makeHeart(this)" class="heart fa-regular fa-heart absolute right-5 top-5 text-xl text-white cursor-pointer "></i>
         <div class="mt-2  bg-white  pl-3">
@@ -128,13 +182,20 @@ function markaFilter() {
         </div>
         <p class="dark:text-gray-800 pb-4 pl-3">${item.il}, ${item.mator}, <span style=" color: ${item.reng};">${item.reng}<span></p>
         </div>`
-)
+    )
 
-cards.innerHTML = kod
-    
+    cards.innerHTML = kod
+
 }
-function makeHeart(element){
+
+function showMore() {
+    cardsNum += 4
+    showBtn.style.display = cardsNum > arr.length ? 'none' : 'block'
+    printCards()
+}
+
+function makeHeart(element) {
     arr.status = !arr.status
     element.style.color = arr.status ? "red" : "white"
-    
+
 }
